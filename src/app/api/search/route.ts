4727c894e044b992bb9f2602ca0fd90f8a39131c
Taylor Gregoire-Wright <taylor.gregoirewright@outlook.com>
2023-04-18 19:30:11 -0700

    Delete code for simple search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,19 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  const objectType = new URL(request.url).searchParams.get("objectType");
-  if (objectType) {
-    const url = `https://${process.env.NEXT_PUBLIC_HOSTNAME}/api/v1/ontologies/${process.env.ONTOLOGYRID}/objects/${objectType}/search/`;
-    return await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.TOKEN}`,
-      },
-      body: JSON.stringify(body),
-    })
-      .then((resp) => resp.json())
-      .then((respJson) => NextResponse.json(respJson));
-  }
+  return NextResponse.json(undefined);
 }
--- src/app/simple/page.tsx
+++ src/app/simple/page.tsx
@@ -1,57 +1,14 @@
 "use client";
 
 import LoadingSpinner from "@/components/loadingSpinner";
-import ResultsList from "@/components/resultsList";
-import SearchBar from "@/components/searchbar";
-import { Query, VehicleRecall } from "@/types";
 import { Inter } from "next/font/google";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function SimpleSearch() {
   const objectType = "VehicleRecall";
-  const pageSize = 10;
-  const [queryTerm, setQueryTermTerm] = useState<string>("");
-  const [results, setResults] = useState<VehicleRecall[]>([]);
   const [isSearching, setIsSearching] = useState(false);
-  const [nextPageToken, setNextPageToken] = useState<string | undefined>();
-
-  const fetchResults = useCallback(async () => {
-    const query: Query = {
-      type: "allTerms",
-      field: "properties.subject",
-      value: queryTerm,
-    };
-
-    fetch(`/api/search?objectType=${objectType}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query, pageSize, pageToken: nextPageToken }),
-    })
-      .then((resp) => resp.json())
-      .then((respJson) => {
-        setResults([...results, ...respJson["data"]]);
-        setNextPageToken(respJson["nextPageToken"]);
-        setIsSearching(false);
-      });
-  }, [queryTerm, nextPageToken, results]);
-
-  function handleSearch() {
-    if (queryTerm !== "") {
-      setIsSearching(true);
-      setResults([]);
-      setNextPageToken(undefined);
-      fetchResults();
-    }
-  }
-
-  useEffect(() => {
-    if (queryTerm === "") {
-      setResults([]);
-      setNextPageToken(undefined);
-    }
-  }, [queryTerm]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-12 p-24">
@@ -70,19 +27,11 @@ export default function SimpleSearch() {
           objects
         </p>
       </div>
-      <SearchBar
-        queryTerm={queryTerm}
-        handleSearch={handleSearch}
-        setQueryTerm={setQueryTermTerm}
-        isSearching={isSearching}
-      />
-      <ResultsList results={results} />
-      {(nextPageToken || isSearching) && (
+      {isSearching && (
         <button
           className="w-30 flex justify-center rounded-md border border-transparent bg-cyan-100 px-4 py-2 text-center text-sm font-medium text-cyan-800 shadow-sm hover:bg-cyan-200 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2"
           onClick={() => {
             setIsSearching(true);
-            fetchResults();
           }}
         >
           {isSearching ? <LoadingSpinner colorVariant="cyan" /> : "Load More"}
